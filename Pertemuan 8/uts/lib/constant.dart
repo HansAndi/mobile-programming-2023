79b9345e9e31cb72b82e121d5c804516c5daf7e7
HansAndi <hansandiwijaya@gmail.com>
2023-11-01 19:01:56 +0700

    theme

--- Pertemuan 8/uts/lib/constant.dart
+++ Pertemuan 8/uts/lib/constant.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 OutlineInputBorder defaultOutlineInputBorder = OutlineInputBorder(
-  borderRadius: BorderRadius.circular(14),
+  borderRadius: BorderRadius.circular(15),
   borderSide: const BorderSide(color: Color(0xFFF3F3F3)),
 );
